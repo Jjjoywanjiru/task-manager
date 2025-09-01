a244bd64f2ca34df838e838739f74e31acc13c64
joymakesyouhappy <128835948+joymakesyouhappy@users.noreply.github.com>
2025-09-01 11:32:18 +0300

    Connected the backend to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,32 @@
+import { useEffect, useState } from "react";
+import API from "./api";
+
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    // fetch tasks from backend
+    API.get("tasks/")
+      .then((res) => {
+        setTasks(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
   return (
     <div>
       <h1>Task Manager Frontend</h1>
-      <p>React is working!</p>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {task.title} - {task.completed ? "✅ Done" : "⏳ Pending"}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default App;
-
